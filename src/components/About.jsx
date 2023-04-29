ec9501f205d4fb1c031042bad6368f7230f926b4
rubelrocks <121501436+programmingherorubel@users.noreply.github.com>
2023-04-30 00:40:50 +0600

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,3 +11,9 @@ const About = () => {
 };
 
 export default About;
+
+// VITE_APIKEY= AIzaSyCsOuFmUf8stPMjh6w60j3Lnxu_7ELStNQ
+// VITE_AUTHDOMAIN= firstproject-4ce02.firebaseapp.com
+// VITE_PROJECTID= firstproject-4ce02
+// VITE_STORAGEBUCKET= firstproject-4ce02.appspot.com
+// VITE_MESSAGINGSENDERID= 48448439838
